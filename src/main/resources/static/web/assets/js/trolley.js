376cd50a51e6911f2abcdf395db68c8438ce50c7
Brandon Wasilewicz <branwasi9021@gmail.com>
2022-09-19 18:14:12 -0300

    carrito de compras

--- src/main/resources/static/web/assets/js/trolley.js
+++ src/main/resources/static/web/assets/js/trolley.js
@@ -4,35 +4,24 @@ const app = Vue.
         data() {
             return {
                 select: "selectStep1",
-                
-
-
+                productStorage:[]
             }
         },
         created() {
-         
-
-
-
+            // this.productStorage = localStorage.products;   
+            this.productStorage = JSON.parse(localStorage.getItem('products'))  
         },
         methods: {
-     
+            
             selectStep (value){      
                 this.select = value
-
                 var valueStep = document.getElementById(value)
-                
-            
                 valueStep.classList.remove("filterSelect") 
-
             },
 
             backStep (value) {
-                
                 var valueStep = document.getElementById(value)
                 this.select = value
-                
-
                 if ( this.select == 'selectStep1') {
                     var valueStep = document.getElementById('selectStep2')
                     valueStep.classList.add("filterSelect") 
@@ -42,25 +31,23 @@ const app = Vue.
                     var valueStep = document.getElementById('selectStep3')
                     valueStep.classList.add("filterSelect") 
 
-                } 
-                                       
+                }                      
              
+            },
+            formattedNumber(balance){
+                return balance = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'}).format(balance)
+            },
+            sendProducts(){
+                axios.post('/api/bills',this.productStorage)
+                .then(() => {
+                    this.productStorage = []
+                })
+            },
+        },
+        computed: {
+            totalAPagar() {
+                return this.formattedNumber(this.productStorage.reduce((acc, item) => acc + item.quantity * item.price, 0));
             }
-
-
-          
-
-
-
-
+            
         }
     }).mount('#app');
-
-/*     removeDisableSection(id1,id2){
-        let disableSectionColumn1 = document.getElementById(id2)
-        let button = document.getElementById(id1)
-        disableSectionColumn1.classList.remove("disableSection")
-        if(button){
-            button.classList.add("d-none")
-        }
-    }, */
